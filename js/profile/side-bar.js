38ac86bdcc879251883a9ca6b0d12c9abdd4b7b1
waleedsalah4 <walidsalah652@gmail.com>
2022-03-17 19:45:25 +0200

    add play-podcast page

--- js/profile/side-bar.js
+++ js/profile/side-bar.js
@@ -4,15 +4,18 @@ const barIcons = document.querySelectorAll('.bar-icons');
 const barIcon = document.querySelector('.bar-icon');
 const barLinks = document.querySelectorAll('.sideBar ul .bar-item');
 
+const toggleIcon = document.querySelector('.bar-icon-container') 
+
 
 
 //event handlers
 
 //1)open and close side bar functionality
-bar.addEventListener('click',function(e){
-    if((e.target.matches('svg') || e.target.matches('path') )&& e.target.classList.contains('bar-icon')){ 
-        bar.classList.toggle('opened');
-    }
+toggleIcon.addEventListener('click',function(e){
+    // if((e.target.matches('svg') || e.target.matches('path') )&& e.target.classList.contains('bar-icon')){ 
+    //     
+    // }
+    bar.classList.toggle('opened');
 
     if(!bar.classList.contains('opened')){
         barLinks.forEach(ln=>ln.classList.remove('highlight')) ;
